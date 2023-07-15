b3446bc1554eca3bd0e0ced24c925c3554179484
yymin1022 <yymin1022@gmail.com>
2023-07-15 17:51:48 +0900

    Update index.ts : Applied Octokit Util

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,40 @@
-import dotenv from "dotenv";
-import {Octokit} from "octokit";
-
-dotenv.config();
-const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
-
-let octokit: Octokit;
-
-const authTest = () => {
-    octokit = new Octokit({
-        auth: GITHUB_TOKEN
-    });
-}
+import * as OctoUtil from "./OctokitUtil";
 
 // https://docs.github.com/ko/rest/metrics/statistics?apiVersion=2022-11-28#get-all-contributor-commit-activity
 const getCommitCount = () => {
-    octokit.request(
+    OctoUtil.sendOctoAPI(
         "GET /repos/{owner}/{repo}/stats/contributors",
         {
             owner: "GDSC-CAU",
             repo: "GDSC-SPACE",
             headers: {"X-GitHub-Api-Version": "2022-11-28"}
-        })
-        .then(res => {
-            console.log(res["data"]);
-        })
+        }
+    );
 }
 
 // https://docs.github.com/ko/rest/orgs/orgs?apiVersion=2022-11-28#get-an-organization
 const getOrganizationInfo = () => {
-    octokit.request(
+    OctoUtil.sendOctoAPI(
         "GET /orgs/{org}",
         {
             org: "GDSC-CAU"
-        })
-        .then(res => {
-            console.log(res["data"]);
-        })
+        }
+    );
 }
 
 // https://docs.github.com/ko/rest/orgs/members?apiVersion=2022-11-28#list-organization-members
 const getOrganizationMembers = () => {
-    octokit.request(
+    OctoUtil.sendOctoAPI(
         "GET /orgs/{org}/members",
         {
             org: "GDSC-CAU",
             per_page: 100
-        })
-        .then(res => {
-            let memberArr: Array<string> = [];
-            res["data"].forEach((memberItem) => {
-                memberArr.push(memberItem["login"]);
-            });
-            console.log(memberArr);
-        })
+        }
+    );
 }
 
-authTest();
+OctoUtil.initOctokit();
 
 getOrganizationInfo();
 getOrganizationMembers();
-
 getCommitCount();
