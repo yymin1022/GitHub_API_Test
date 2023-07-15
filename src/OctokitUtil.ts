46bb0434207b4721ae566aa63e03a5ed976a3220
yymin1022 <yymin1022@gmail.com>
2023-07-15 17:57:19 +0900

    Update Octokit Util : Improved API Transaction Function

--- src/OctokitUtil.ts
+++ src/OctokitUtil.ts
@@ -6,6 +6,11 @@ const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
 
 let octokit: Octokit
 
+export interface OctoResult {
+    success: boolean,
+    result: any
+}
+
 export const initOctokit = () => {
     if(octokit === undefined){
         octokit = new Octokit({
@@ -15,11 +20,20 @@ export const initOctokit = () => {
 }
 
 export const sendOctoAPI = async (path: string, data: any) => {
+    let resultData: OctoResult = {
+        success: false,
+        result: {}
+    }
+
     await octokit.request(path, data)
         .then(res => {
-            console.log(res["data"]);
+            resultData.success = true;
+            resultData.result = res["data"];
         })
         .catch((err) => {
-            console.log(err);
+            resultData.success = false;
+            resultData.result = err.toString();
         })
+
+    return resultData;
 }
