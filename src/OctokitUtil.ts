9eb0133271f1f86987af79224f48ce6cad5b03db
yymin1022 <yymin1022@gmail.com>
2023-07-15 17:51:20 +0900

    Update Octokit Util : Applied Promise to Octokit Initialization

--- src/OctokitUtil.ts
+++ src/OctokitUtil.ts
@@ -6,12 +6,10 @@ const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
 
 let octokit: Octokit
 
-export const initOctokit = () => {
-    if(octokit !== undefined){
-        octokit = new Octokit({
-            auth: GITHUB_TOKEN
-        });
-    }
+export const initOctokit = async () => {
+    octokit = await new Octokit({
+        auth: GITHUB_TOKEN
+    });
 }
 
 export const sendOctoAPI = async (path: string, data: any) => {
