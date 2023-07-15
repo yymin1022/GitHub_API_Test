d67926e5d3a648cd43f77dc92516501e0f3695b9
yymin1022 <yymin1022@gmail.com>
2023-07-15 17:35:30 +0900

    Update OctokitUtil : Added Octokit Initialization

--- src/OctokitUtil.ts
+++ src/OctokitUtil.ts
@@ -1,5 +1,17 @@
-const initOctokit = () => {
+import dotenv from "dotenv";
+import {Octokit} from "octokit";
 
+dotenv.config();
+const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
+
+let octokit: Octokit
+
+const initOctokit = () => {
+    if(octokit !== undefined){
+        octokit = new Octokit({
+            auth: GITHUB_TOKEN
+        });
+    }
 }
 
 const getOctokitAPI = (path: string, data: object) => {
